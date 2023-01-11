7e42bda01948fce2777c4c2a9e567585bc80418d
FernandoMoyano <fernandomoyano21@gmail.com>
2023-01-11 15:15:19 -0300

    sidebar-sidebarcontext

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext } from "react";
+//import Link
+import { Link } from "react-router-dom";
+//import icons
+import { IoMdFastforward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+//import components
+import CartItem from "../components/CartItem";
+//import sidebarcontext
+import { SidebarContext } from "../contexts/SidebarContext";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+	const { isOpen, handleClose } = useContext(SidebarContext);
+	return <div>Sidebar</div>;
 };
 
 export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+//product provider
 import ProductProvider from "./contexts/ProductContext";
+//sidebar provider
+import SidebarProvider from "./contexts/SidebarContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<ProductProvider>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</ProductProvider>
+	<SidebarProvider>
+		<ProductProvider>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</ProductProvider>
+	</SidebarProvider>
 );
